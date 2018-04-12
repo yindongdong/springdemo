f8bdf5198f4cdb363db6d148eb8ddf91e00a2549
yindongdong <282887866@qq.com>
2018-04-12 21:47:33 +0800

    一个tomcat部署多个项目配置

--- spring-api/src/main/java/com/winter/springapi/SpringApiApplication.java
+++ spring-api/src/main/java/com/winter/springapi/SpringApiApplication.java
@@ -3,6 +3,8 @@ package com.winter.springapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,14 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-public class SpringApiApplication {
+public class SpringApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringApiApplication.class);
+	}
 }
